7c35135618d512c235ef684a07fa43d759c911f4
shuhab <Shuhab12345@hotmail.com>
2020-02-25 23:08:38 +0100

    Added Player movement on 1 Thread. Added Bitmap with 1 working using canvas

--- Game/GameWindow.xaml.cs
+++ Game/GameWindow.xaml.cs
@@ -1,34 +1,75 @@
 ﻿using System.Windows;
-using System.Drawing;
+using System.Drawing.Imaging;
 using System;
-
-// For some reason bitmaps cannot be used unless the newest version is found.
-// which can be found by pressing on Graphics and installing from below!!
-/*
-public abstract void (Graphics g){
-
-}
-*/
+using System.Drawing;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Diagnostics;
+using System.Windows.Threading;
 
 namespace Game
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class GameWindow : Window {
 
+        int xPos = 0;
+        int yPos = 0;
 
+        Player player = new Player(0,0);
 
         public GameWindow() {
+           
+            
             InitializeComponent();
-            Bitmap image1 = new Bitmap(@"C:\Users\shuha\Source\Repos\Game\Game\Bitmaps\Adventurer\Individual Sprites\adventurer-attack1-00.png", true);
+
+            // Game tick 
+            DispatcherTimer Timer = new DispatcherTimer();
+
+            Timer.Tick += new EventHandler(gameUpdate);
+            Timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            Timer.Start();
+
+
+            Scene.Children.Add(player);
+
+
+
+
+
+
+
+
+
 
 
         }
 
+
+        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
+
+            if (e.Key == System.Windows.Input.Key.W) { this.yPos -= 3; Trace.WriteLine("UP "  + yPos); }
+            if (e.Key == System.Windows.Input.Key.S) { this.yPos += 3; Trace.WriteLine("DOWN" + yPos); }
+            if (e.Key == System.Windows.Input.Key.D) { this.xPos += 3; Trace.WriteLine("LEFT" + xPos); }
+            if (e.Key == System.Windows.Input.Key.A) { this.xPos -= 3; Trace.WriteLine("RIGHT"+ xPos); }
+
+
+        }
+
+        private void gameUpdate(object sender, EventArgs e) {
+
+            Canvas.SetLeft(player, xPos);
+            Canvas.SetTop(player, yPos);
+           
+           
        
 
 
-    
+
+
+        }
+
+
+
+
     }
 }
---
+++ Game/Player.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace Game {
+
+  
+
+    class Player : Image {
+
+        // Movement values for the player 
+        private readonly int height = 50;
+        private readonly int width = 50;
+
+        private int startPositionX = 0;
+        private int startPositionY = 0;
+        // A tick for each player 
+
+           DispatcherTimer Timer = new DispatcherTimer();
+
+
+
+
+        public Player(int startPositionX, int startPositionY) {
+
+            BitmapImage action = new BitmapImage();
+            
+            action.BeginInit();
+            action.UriSource = new Uri(@"C:\Users\shuha\Source\Repos\Game\Game\Bitmaps\Adventurer\Individual Sprites\adventurer-attack1-00.png", UriKind.RelativeOrAbsolute);
+            action.EndInit();
+            this.Stretch = Stretch.UniformToFill;
+            this.Source = action;
+            
+            this.Width = width;
+            this.Height = height;
+
+
+            Canvas.SetLeft(this, this.startPositionX);
+            Canvas.SetTop(this, this.startPositionY);
+
+
+            
+            Timer.Tick += new EventHandler(update);
+            // Set the update rate      //Y  H  M  S  MS
+            Timer.Interval = new TimeSpan(0, 0, 0, 0, 100); // 60 FPS 
+            Timer.Start();
+            
+            
+          
+
+        }
+
+        
+  
+
+
+        
+        private void update(object sender, EventArgs e) {
+
+            Trace.WriteLine("Player UP");
+            Canvas.SetLeft(this,this.startPositionX);
+            Canvas.SetTop(this, this.startPositionY);
+
+        
+        }
+        
+
+    }
+}
