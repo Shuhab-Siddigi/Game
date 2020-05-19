12520a8d0383bef26511b1e939864b9216b4d593
Shuhab <Shuhab12345@hotmail.com>
2020-05-19 19:24:09 +0200

    Created list as wall instead. can be used as a Lvl

--- Game/GameWindow.xaml.cs
+++ Game/GameWindow.xaml.cs
@@ -13,35 +13,36 @@ using Game.Game_Object_Classes;
 namespace Game {
 
     public partial class GameWindow : Window {
-       
+
         CollisionDetection collision = new CollisionDetection();
 
 
-        Slime slime = new Slime(100,150,50,50);
-        Player player = new Player(10,450,50,50);
-        Wall TopWall = new Wall(Block.blocksize*0, Block.blocksize*0, 39, 1);
-        Wall BottomWall = new Wall(Block.blocksize * 0, Block.blocksize * 29, 39, 1);
-        Wall LeftWall = new Wall(Block.blocksize * 0, Block.blocksize * 0, 1, 30);
-        Wall RightWall = new Wall(Block.blocksize * 38, Block.blocksize * 0, 1, 30);
+        Slime slime = new Slime(100, 150, 40, 40);
+        Player player = new Player(10, 450, 50, 50);
 
+        List<Wall> Walls = new List<Wall>() {
 
-        Wall MidWall1 = new Wall(Block.blocksize * 3, Block.blocksize * 26, 6, 1);
-        Wall MidWall2 = new Wall(Block.blocksize * 23, Block.blocksize * 20, 1, 5);
-        Wall MidWall3 = new Wall(Block.blocksize * 12, Block.blocksize * 26, 6, 1);
-        Wall MidWall4 = new Wall(Block.blocksize * 9, Block.blocksize * 23, 6, 1);
-        Wall MidWall5 = new Wall(Block.blocksize * 20, Block.blocksize * 26, 6, 1);
-        Wall MidWall6 = new Wall(Block.blocksize * 17, Block.blocksize * 23, 6, 1);
-        Wall MidWall7 = new Wall(Block.blocksize * 1, Block.blocksize * 20, 28, 1);
-        Wall MidWall8 = new Wall(Block.blocksize * 11, Block.blocksize * 24 , 1, 5);
-        Wall MidWall9 = new Wall(Block.blocksize * 0, Block.blocksize * 0, 0, 0);
-        Wall MidWall10 = new Wall(Block.blocksize * 0, Block.blocksize * 0,0, 0);
-
-
+            new Wall(Block.blocksize* 0, Block.blocksize* 0, 39, 1), // Top wall
+            new Wall(Block.blocksize* 0, Block.blocksize* 29, 39, 1), // Bottom wall
+            new Wall(Block.blocksize * 0, Block.blocksize * 0, 1, 30), // Left Wall
+            new Wall(Block.blocksize* 38, Block.blocksize* 0, 1, 30), // Right Wall
+            new Wall(Block.blocksize* 3, Block.blocksize* 26, 6, 1),
+            new Wall(Block.blocksize* 23, Block.blocksize* 20, 1, 5),
+            new Wall(Block.blocksize* 12, Block.blocksize* 26, 6, 1),
+            new Wall(Block.blocksize* 9, Block.blocksize* 23, 6, 1),
+            new Wall(Block.blocksize* 20, Block.blocksize* 26, 6, 1),
+            new Wall(Block.blocksize* 17, Block.blocksize* 23, 6, 1),
+            new Wall(Block.blocksize* 1, Block.blocksize* 20, 28, 1),
+            new Wall(Block.blocksize* 11, Block.blocksize* 24, 1, 5),
+            new Wall(Block.blocksize* 28, Block.blocksize* 23, 1, 5),
+            new Wall(Block.blocksize* 30, Block.blocksize* 26, 5, 1),
+            new Wall(Block.blocksize* 33, Block.blocksize* 23, 5, 1),
+        };
 
         public GameWindow() {
 
             InitializeComponent();
- 
+
             startGame();
         }
 
@@ -51,59 +52,39 @@ namespace Game {
             CompositionTarget.Rendering += OnUpdate;
             Scene.Width = GlobalSettings.ScreenWidth;
             Scene.Height = GlobalSettings.ScreenHeight;
-            Window.Width = GlobalSettings.ScreenWidth-4;
-            Window.Height = GlobalSettings.ScreenHeight-1;
+            Window.Width = GlobalSettings.ScreenWidth - 4;
+            Window.Height = GlobalSettings.ScreenHeight - 1;
 
             // Add objects to the screen
             AddSlime(slime);
             AddPlayer(player);
-            AddWall(TopWall);
-            AddWall(BottomWall);
-            AddWall(LeftWall);
-            AddWall(RightWall);
-            AddWall(MidWall1);
-            AddWall(MidWall2);
-            AddWall(MidWall3);
-            AddWall(MidWall4);
-            AddWall(MidWall5);
-            AddWall(MidWall6);
-            AddWall(MidWall7);
-            AddWall(MidWall8);
-            AddWall(MidWall9);
-            AddWall(MidWall10);
+
+            foreach (Wall wall in Walls) {
+                AddWall(wall);
+            }
         }
+
 
         private void OnUpdate(object sender, EventArgs e) {
 
-            Input.Update();
+                Input.Update();
+
+                // Default settings
+                slime.DefaultSettings();
+                player.DefaultSettings();
+
+                // Update Settings
+                foreach (Wall wall in Walls) {
+                    wall.Update(player, slime);
+                }
+
+                player.Update();
+                slime.Update();
             
-            // Default settings
-            slime.DefaultSettings();
-            player.DefaultSettings(); 
-
-            // Update Settings
-
-            TopWall.Update(player,slime);
-            BottomWall.Update(player, slime);  
-            LeftWall.Update(player, slime);       
-            RightWall.Update(player, slime);
-            MidWall1.Update(player, slime);
-            MidWall2.Update(player, slime);
-            MidWall3.Update(player, slime);
-            MidWall4.Update(player, slime);
-            MidWall5.Update(player, slime);
-            MidWall6.Update(player, slime);
-            MidWall7.Update(player, slime);
-            MidWall8.Update(player, slime);
-            MidWall9.Update(player, slime);
-            MidWall10.Update(player, slime);
-
-            player.Update();
-            slime.Update();
         }
 
         private void AddWall(Wall wall) {
-           
+
             foreach (Block block in wall) {
                 Scene.Children.Add(block);
             }
@@ -125,6 +106,6 @@ namespace Game {
             Scene.Children.Add(slime.HitBoxRender);
         }
 
-       
+
     }
 }
